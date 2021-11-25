6a07a33f912279b36f24fc53db198960a6a84fd8
YuliaPol <62880025+YuliaPol@users.noreply.github.com>
2021-11-25 11:31:08 +0200

    ass common nps page

--- js/carts-delivery.js
+++ js/carts-delivery.js
@@ -1,37 +1,76 @@
 /** Bar charts */
+if(typeof barChartData !== "undefined"){
+    if(barChartData.length > 0){
+        for (let i = 0; i < barChartData.length; i++) {
+            drawBarChart(barChartData[i]);
+        }
+    }
+}
 
-bar1VallArr.map((v, i) => {
-    $($('.pink-bar .bar-content')[i]).css('height', `${v}%`);
-    $($('.pink-bar .count-val')[i]).text(`${v}`);
-});
-
-bar2VallArr.map((v, i) => {
-    $($('.purple-bar .bar-content')[i]).css('height', `${v}%`);
-    $($('.purple-bar .count-val')[i]).text(`${v}`);
-});
-
-bar3VallArr.map((v, i) => {
-    $($('.blue-bar .bar-content')[i]).css('height', `${v}%`);
-    $($('.blue-bar .count-val')[i]).text(`${v}`);
-});
-
-bar4VallArr.map((v, i) => {
-    $($('.green-bar .bar-content')[i]).css('height', `${v}%`);
-    $($('.green-bar .count-val')[i]).text(`${v}`);
-});
-
-/** Show Tooltips */
-
-$('.bar-content').hover(function () {
-    // mouse enters
-    $(this).children('.bar-tooltip').css({
-        'z-index': '50',
-        'display': 'block'
-    })
-}, function () {
-    // mouse leaves
-    $(this).children('.bar-tooltip').css({
-        'z-index': '0',
-        'display': 'none'
-    })
-});
+function drawBarChart(barEl){
+    let barData = barEl.data;
+    let maxValue = 10;
+    for (let i = 0; i < barData.length; i++) {
+        if(parseInt(barData[i].progress) > maxValue){
+            maxValue = parseInt(barData[i].progress);
+        }
+    }
+    var maxAxes = maxValue + maxValue*0.2;
+    var minAxes = 0;
+    var axes = new Array();
+    var step = 5;
+    while ((maxValue + step)/step > 9 || step>10000){
+        if(step >=200) {
+            step += 100;
+        }
+        else if(step >=50) {
+            step += 50;
+        }
+        else if(step >=30) {
+            step += 10;
+        }
+        else {
+            step += 5;
+        }
+    }
+    var axesValue = minAxes;
+    for (let i = 0; axesValue < maxAxes + step; i++) {
+        axes.push(axesValue);
+        axesValue = axesValue + step;
+    }
+    let percent = 100/axesValue;
+    let barHtml = "";
+    for (let i = 0; i < barData.length; i++) {
+        barHtml += 
+        `<div class="bar five-el-bar">
+            <div class="bar-bg">
+                <div class="bar-content pink-bar-content" style="height:0%;background:${barEl.color}">
+                    <div class="bar-tooltip">
+                        <p class="tooltip-count"><span class="count-val">${barData[i].progress}</span>шт</p>
+                    </div>
+                </div>
+            </div>
+            <div class="bar-name">${barData[i].labelText}</div>
+        </div>`; 
+    }
+    barHtml += `<div class="y-scale">`;
+    for (let i = 0; i < axes.length; i++) {
+        if(i === 0){
+            barHtml += `<div class="scale"><span class="y-scale-number"></span></div>`;
+        } else {
+            barHtml += `<div class="scale"><span class="y-scale-number">${axes[i]} шт.</span></div>`;
+        }
+    }
+    barHtml += 
+    `</div>`;
+    if($(barEl.element).length > 0){
+        $(barEl.element).html(barHtml);
+        let bars = $(barEl.element).find('.bar');
+        setTimeout(() => {
+            for (let i = 0; i < bars.length; i++) {
+                let percentItem = Math.round(percent*parseInt(barData[i].progress)) + '%';
+                $(bars[i]).find('.bar-content').css('height', percentItem);
+            }
+        }, 100);
+    }
+}
